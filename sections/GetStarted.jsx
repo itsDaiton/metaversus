d55cafa26bdd89c1cdbdc9acd0211010df9bcba5
David Poslušný <david.poslusny@email.cz>
2023-02-23 14:17:49 +0100

    features + get started sections

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,10 +1,51 @@
 'use client';
 import React from 'react'
+import { motion } from 'framer-motion';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { TitleText, TypingText } from '../components/CustomTexts';
+import { features } from '../constants/data'
+import StartSteps from '../components/StartSteps';
 
 const GetStarted = () => {
   return (
-    <section>
-      Get Started
+    <section className='sm:p-16 xs:p-8 px-6 py-12 relative z-10'>
+      <motion.div
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{
+          once: false,
+          amount: 0.25
+        }}
+        className='2xl:max-w-[1280px] w-full mx-auto flex lg:flex-row flex-col gap-8'
+      >
+        <motion.div
+          variants={planetVariants('left')}
+          className='flex-1 flex justify-center items-center'
+        >
+          <img
+            src='/get-started.png'
+            alt='get-started'
+            className='w-[90%] h-[90%] object-contain'
+          />
+        </motion.div>
+        <motion.div
+          variants={fadeIn('left', 'tween', 0.2, 1)}
+          className='flex-[0.75] flex justify-center flex-col'
+        >
+          <TypingText title='| How Metaverusus Works'/>
+          <TitleText title={<>Get started with just a few click</>} />
+          <div className='mt-[31px] flex flex-col max-w-[370px] gap-[24px]'>
+            {features.map((feature, index) => (
+              <StartSteps
+                key={feature}
+                number={index + 1}
+                text={feature}
+              />
+            ))}
+          </div>
+        </motion.div>
+      </motion.div>
     </section>
   )
 }
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,10 +1,51 @@
 'use client';
 import React from 'react'
+import { motion } from 'framer-motion';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { TitleText, TypingText } from '../components/CustomTexts';
+import { newFeatures } from '../constants/data'
+import StartSteps from '../components/StartSteps';
+import NewFeatures from '@/components/NewFeatures';
 
 const WhatsNew = () => {
   return (
-    <section>
-      Whats New
+    <section className='sm:p-16 xs:p-8 px-6 py-12 relative z-10'>
+      <motion.div
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{
+          once: false,
+          amount: 0.25
+        }}
+        className='2xl:max-w-[1280px] w-full mx-auto flex lg:flex-row flex-col gap-8'
+      >
+        <motion.div
+          variants={fadeIn('right', 'tween', 0.2, 1)}
+          className='flex-[0.75] flex justify-center flex-col'
+        >
+          <TypingText title="| What's New"/>
+          <TitleText title={<>What's new about Metaversus?</>} />
+          <div className='mt-[48px] flex flex-wrap justify-between gap-[24px]'>
+            {newFeatures.map((feature) => (
+              <NewFeatures
+                key={feature.title}
+                {...feature}
+              />
+            ))}
+          </div>
+        </motion.div>
+        <motion.div
+          variants={planetVariants('right')}
+          className='flex-1 flex justify-center items-center'
+        >
+          <img
+            src='/whats-new.png'
+            alt='whats-new'
+            className='w-[90%] h-[90%] object-contain'
+          />
+        </motion.div>
+      </motion.div>
     </section>
   )
 }
