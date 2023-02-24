17dce068a84c58d871066a5603858f88a8dfcfdd
David Poslušný <david.poslusny@email.cz>
2023-02-24 16:14:54 +0100

    final changes

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import { Hero, About, Explore, GetStarted, WhatsNew, World, Insights, Feedback } from '../sections'
+import { Navbar, Footer } from '../components'
+
+const Page = () => {
+  return (
+    <div className='bg-primary-black overflow-hidden'>
+      <Navbar/>
+      <Hero/>
+      <div className='relative'>
+        <About/>
+        <div className='gradient-03 z-0'/>
+        <Explore/>
+      </div>
+      <div className='relative'>
+        <GetStarted/>
+        <div className='gradient-04 z-0'/>
+        <WhatsNew/>
+      </div>
+      <World/>
+      <div className='relative'>
+        <Insights/>
+        <div className='gradient-04 z-0'/>
+        <Feedback/>
+      </div>
+      <Footer/>   
+    </div>
+  )
+}
+
+export default Page
